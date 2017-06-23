0aa0b5cc41460601a7fc9cceda9f1e84171a0f41
Wei Ji <weiji1994@Hotmail.com>
2017-06-23 14:00:51 +1200

    Tidy up ICESAT stuff and upload metadata files
    Restructure folders so that data only in data folder, and scripts in code/scripts folder. Initial commit for helper wget bash script.
    
    The /data/icesat/GLA12.034/*/*.DAT files are too big (~34GB), so only hosting them locally. Anyone wanting a copy of the binary files can obtain it from NSIDC until August 2017, or get if from elsewhere.

---
+++ code/scripts/icesat_dwn.sh
@@ -0,0 +1,19 @@
+#Bash script based off https://nsidc.org/support/faq/what-options-are-available-bulk-downloading-data-https-earthdata-login-enabled WGET Instructions - for command line in Mac and Unix/Linux
+#
+#generate .netrc file from interactive prompt
+touch ~/.netrc  #note that this file will have to exist in your linux home directory
+read -p "Eartdata login username: " user
+read -s -p "Eartdata login password: " pass
+echo "machine urs.earthdata.nasa.gov login $user password $pass" >> ~/.netrc
+chmod 0600 ~/.netrc
+
+#create cookies file
+touch .urs_cookies
+#trigger authentication first
+wget -c -P ../../data/icesat --load-cookies .urs_cookies --save-cookies .urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on -r --reject "index.html*,icons" -nH -np --cut-dirs=1 -e robots=off "https://n5eil01u.ecs.nsidc.org/GLAS/GLA12.034/"
+#does actual download
+wget -c -P ../../data/icesat --load-cookies .urs_cookies --save-cookies .urs_cookies --keep-session-cookies --no-check-certificate --auth-no-challenge=on -r --reject "index.html*,icons" -nH -np --cut-dirs=1 --tries=5 -e robots=off "https://n5eil01u.ecs.nsidc.org/GLAS/GLA12.034/"
+
+#final tidy up
+rm .urs_cookies
+sed -i '$d' ~/.netrc
---
+++ data/README.md
@@ -0,0 +1,7 @@
+# Data Sources
+
+## [ICESAT](https://en.wikipedia.org/wiki/ICESat)
+### GLAS/ICESat L2 Antarctic and Greenland Ice Sheet Altimetry Data (Binary), Version 34
+- https://nsidc.org/data/GLA12/versions/34
+- https://n5eil01u.ecs.nsidc.org/GLAS/GLA12.034/
+- Zwally, H. J., R. Schutz, C. Bentley, J. Bufton, T. Herring, J. Minster, J. Spinhirne, and R. Thomas. 2014. GLAS/ICESat L2 Antarctic and Greenland Ice Sheet Altimetry Data, Version 34. [Whole continent]. Boulder, Colorado USA. NASA National Snow and Ice Data Center Distributed Active Archive Center. doi: http://dx.doi.org/10.5067/ICESAT/GLAS/DATA225. [2017-06-22].
