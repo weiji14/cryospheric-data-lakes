4431d449bebdc1a83012bc0958384c1ebac1fa86
Wei Ji <weiji14@users.noreply.github.com>
2017-06-30 18:14:41 +1200

    Branch off to devel/aioftp for investigating asynchronous cryosat dl
    Very experimental and buggy get_cryosat.py at the moment. Much reliant on riohttp framework from https://hackernoon.com/asyncio-for-the-working-python-developer-5c468e6e2e8e. TODO port to rioftp instead...
    
    Temporarily backport to using atom stable 1.18.0 instead of 1.19 beta due to hydrogen truncated output bubble issue https://github.com/nteract/hydrogen/issues/898

--- code/scripts/get_cryosat.py
+++ code/scripts/get_cryosat.py
@@ -1,19 +1,36 @@
+#!/usr/bin/env python3
 ## Python 3 script to download satellite data from FTP sites.
 ## Originally designed for pulling data from European Space Agency (ESA) FTP sites, e.g. for Cryosat-2
 import ftplib
 import getpass
 import os
-import subprocess
+import asyncio
+import aioftp
+import time
 
 print(os.getcwd())
 dataDir = os.path.join(os.getcwd(), 'data')
 ftpSites = {'ESA EO' : "science-pds.cryosat.esa.int"}
 #ftpSites.extend{"NSIDC Earthdata" : "https://n5eil01u.ecs.nsidc.org/GLAS/GLA12.034/"}  #TODO add icesat and other satellites as well?
 
+MAX_CLIENTS = 3
+async def fetch_async(pid, URL):
+    print('Fetch async process {} started'.format(pid))
+    start = time.time()
+    response = await aiohttp.request('GET', URL)
+    datetime = response.headers.get('Date')
+
+    print('Process {}: {}, took: {:.2f} seconds'.format(
+        pid, datetime, time.time() - start))
+
+    response.close()
+    return datetime
+
 for orgAccount, siteURL in ftpSites.items():
     #
     if 'cryosat' in siteURL:
         satName = 'cryosat'
+        pathList = ["SIR_GDR", "2017", "01"]
         path = os.path.join("SIR_GDR", "2017", "01")
 
     #Make directory if not already exist
@@ -27,11 +44,28 @@ for orgAccount, siteURL in ftpSites.items():
     ftp.login(usr, pwd)                                 #login using raw input of username and password
 
     #Get list of stuff in the ftp directory
-    print(ftp.dir())
+    #print(ftp.dir())
     [ftp.cwd(dir) for dir in path.split(os.sep)]        #change to path we want recursively, cross-platform compatible
-    ftp.retrlines('LIST')                               #print list of files in ftp folder
+    #ftp.retrlines('LIST')                               #print list of files in ftp folder
     filenames = ftp.nlst()                              #get list of files in ftp folder
 
-    #Loop to download files recursively
+    #Synchronous loop to download files recursively
+    print('Synchronous:')
+    start = time.time()
     for filename in filenames[:2]:
         ftp.retrbinary('RETR {0}'.format(filename), open(os.path.join(dataDir, satName, path, filename), 'wb').write)
+    print("Process took: {:.2f} seconds".format(time.time() - start))
+
+    async def asynchronous():
+        start = time.time()
+        filepaths = [r"ftp://{0}".format("/".join([siteURL]+pathList+[filename])) for filename in filenames[2:4]]
+        tasks = [asyncio.ensure_future(
+            fetch_async(i, urlpath)) for i, urlpath in enumerate(filepaths)]
+        await asyncio.wait(tasks)
+        print("Process took: {:.2f} seconds".format(time.time() - start))
+
+    #Asynchronous loop to download files recursively
+    print('Asynchronous:')
+    ioloop = asyncio.get_event_loop()
+    ioloop.run_until_complete(asynchronous())
+    ioloop.close()
