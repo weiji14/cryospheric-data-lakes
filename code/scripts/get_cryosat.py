d5d63c5ef482e7df2fb96ab03a205c2d7b2b841f
Wei Ji <weiji14@users.noreply.github.com>
2017-07-01 15:26:49 +1200

    Messy but working commit of async aioftp cryosat download!
    Working but untidy version of python3 asyncio/aioftp code to concurrently download files off the Cryosat ftp server. Classical/standard ftplib code is still in there to get the filenames but aiming to have that removed the next commit.
    
    Currently able to run the whole script using atom-editor's in-built hydrogen via Ctrl+Shift+Alt+P. Output prints to the bottom. To re-run, will have to shutdown hydrogen kernels first using Ctrl+Shift+P (to bring up dialog), type-in shutdown, and press Enter on seeing 'Hydrogen: Shutdown Kernel'. Alternatively, use atom-runner's Alt-R to run the whole thing, but you don't see anything printed out until the very end.
    
    Also added some hopefully helpful info for people to get the ESA Cryosat ftp username and password sorted out.

--- code/scripts/get_cryosat.py
+++ code/scripts/get_cryosat.py
@@ -15,16 +15,31 @@ ftpSites = {'ESA EO' : "science-pds.cryosat.esa.int"}
 
 MAX_CLIENTS = 3
 async def fetch_async(pid, URL):
-    print('Fetch async process {} started'.format(pid))
     start = time.time()
-    response = await aiohttp.request('GET', URL)
-    datetime = response.headers.get('Date')
+    print('Fetch async process {0} started at {1}'.format(pid, start))
 
-    print('Process {}: {}, took: {:.2f} seconds'.format(
-        pid, datetime, time.time() - start))
+    toCdDir = "/".join(URL.split("/")[1:-1])   #the directory inside the ftp server to change_directory (cd) into
+    theFile = URL.split("/")[-1]               #the filename which we wish to get
 
-    response.close()
-    return datetime
+    async with aioftp.ClientSession(siteURL, 21, user=usr, password=pwd) as client:
+        print(await client.get_current_directory())   #print current working directory in ftp
+        await client.change_directory(toCdDir)        #change ftp directory to the folder we want
+        print(await client.get_current_directory())   #print the new current working directory
+
+        #for path, info in (await client.list(recursive=True)):
+        #    print(path, info)
+
+        print("Downloading", theFile)
+        print(await client.is_file(theFile))
+        await client.download(theFile, os.path.join(dataDir, satName, path))                #finally downloads the file we want into specified folder
+        print(theFile, "has been downloaded")
+
+        #for path, info in (await client.list(recursive=True)):
+            #print(path,info)
+            #if info["type"] == "file" and path.suffix == ".HDR":
+            #    await client.download(path)
+
+    print('Process {}, took: {:.2f} seconds'.format(pid, time.time() - start))
 
 for orgAccount, siteURL in ftpSites.items():
     #
@@ -39,6 +54,21 @@ for orgAccount, siteURL in ftpSites.items():
 
     #Connect to ftp site using your own login information
     ftp = ftplib.FTP(siteURL)                           #Connect to top level of the FTP server
+    '''
+        Info below is valid as of 20170701:
+
+        To access Cryosat data, you'll have to obtain an ESA Earth Online login https://earth.esa.int/web/guest/-/how-to-access-cryosat-data-6842
+        TLDR:
+            Registration page is at https://eo-sso-idp.eo.esa.int/idp/umsso20/registration, go there and fill in all your login info.
+
+        HAHAYHTR (haha you have to read):
+            Once you have an ESA EO account, do the 2-day long 'fast registration' https://earth.esa.int/web/guest/pi-community/apply-for-data/fast-registration
+            This is so that you can actually gain access to the ftp servers holding the data.
+            Follow the guidelines there to register for access to practical all the Cryosat datasets you can get your hands on (in case you want them later).
+            Specifically though, you probably just want the SIR_GDR_2 product, which is the most processed Geophysical Data Record Level 2 Cryosat product.
+
+        Once you get an email that tells you your ftp username and password, you can enter it into the fields below! Good luck!!
+    '''
     usr = input("{0} login username".format(orgAccount))
     pwd = getpass.getpass("{0} login password".format(orgAccount))
     ftp.login(usr, pwd)                                 #login using raw input of username and password
@@ -54,18 +84,27 @@ for orgAccount, siteURL in ftpSites.items():
     start = time.time()
     for filename in filenames[:2]:
         ftp.retrbinary('RETR {0}'.format(filename), open(os.path.join(dataDir, satName, path, filename), 'wb').write)
-    print("Process took: {:.2f} seconds".format(time.time() - start))
+    print("Synchronous process took: {:.2f} seconds".format(time.time() - start))
+    #9.88s benchmark
+
+    filepaths = [r"{0}".format("/".join([siteURL]+pathList+[filename])) for filename in filenames[2:4]]
 
     async def asynchronous():
         start = time.time()
-        filepaths = [r"ftp://{0}".format("/".join([siteURL]+pathList+[filename])) for filename in filenames[2:4]]
-        tasks = [asyncio.ensure_future(
-            fetch_async(i, urlpath)) for i, urlpath in enumerate(filepaths)]
-        await asyncio.wait(tasks)
-        print("Process took: {:.2f} seconds".format(time.time() - start))
+
+        tasks = [asyncio.ensure_future(fetch_async(i, urlpath)) for i, urlpath in enumerate(filepaths)]  #Create series of coroutines to execute
+        #tasks = [fetch_async(i, urlpath) for i, urlpath in enumerate(filepaths)]
+        await asyncio.wait(tasks)                                                 #Execute those coroutines concurrently and wait for them to fin
+
+        print("Asynchronous process took: {:.2f} seconds".format(time.time() - start))
+        #17.80s benchmark
 
     #Asynchronous loop to download files recursively
     print('Asynchronous:')
-    ioloop = asyncio.get_event_loop()
-    ioloop.run_until_complete(asynchronous())
-    ioloop.close()
+    if __name__ == '__main__':
+        loop = asyncio.get_event_loop()
+        try:
+            loop.run_until_complete(asynchronous())
+        finally:
+            loop.close()
+    print("end")
