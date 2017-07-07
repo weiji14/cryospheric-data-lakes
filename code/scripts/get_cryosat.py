02f388ae934322192f804021b82ec345a8a490b9
Wei Ji <weiji14@users.noreply.github.com>
2017-07-08 11:24:02 +1200

    Preliminary implementation of smart recursive async download function
    Use a recursive loop in a loop to traverse any number of subfolders akin to wget -r. This is a working copy that is about to be refactored to remove the middle fetch_async function.
    
    Also reverted atom-hydrogen-beta dockerfile from atom-stable back to using atom-beta since truncated output bubble issue is fixed in Atom 1.19-beta3 https://github.com/nteract/hydrogen/issues/898#issuecomment-313798624

--- code/scripts/get_cryosat.py
+++ code/scripts/get_cryosat.py
@@ -14,18 +14,36 @@ dataDir = pathlib.PurePath(pathlib.Path.cwd()).joinpath('data')
 ftpSites = {'ESA EO' : "science-pds.cryosat.esa.int"}
 #ftpSites.extend{"NSIDC Earthdata" : "https://n5eil01u.ecs.nsidc.org/GLAS/GLA12.034/"}  #TODO add icesat and other satellites as well?
 
+async def smart_recursive_dwn(client, pathName, pathFolder):
+    '''
+    Using aioftp client, download what's in pathName (remote) to pathFolder (local)
+    '''
+    #checks first if the pathName references a file or a folder
+    if await client.is_file(pathName):
+        #print((await client.stat(pathName))['size'], pathlib.Path(pathFolder, pathName).stat().st_size)
+        #print(pathlib.Path(pathFolder, pathName).exists(), (await client.stat(pathName))['size'], pathlib.Path(pathFolder, pathName).stat().st_size)
+        if (not pathlib.Path(pathFolder, pathName).exists()) or (int((await client.stat(pathName))['size']) != pathlib.Path(pathFolder, pathName).stat().st_size):
+            start = time.time()
+            print("Downloading", pathName)
+            await client.download(pathName, pathFolder)                #finally downloads the file we want into specified folder
+            print("{} download took {:.2f} seconds".format(pathName, time.time()-start))
+    elif await client.is_dir(pathName):
+        await client.change_directory(pathName)
+        print("Starting recursive download of {0} files in {1}".format(len(await client.list(await client.get_current_directory())), await client.get_current_directory()))
+        for newPath, _ in (await client.list()):
+            await smart_recursive_dwn(client, newPath, pathlib.Path(pathFolder, pathName))                   #we loop inside a loop!
+
 async def fetch_async(sem, pid, posixPath):
     async with sem:        #ensure that we abide by Semaphore limit. I.e. do not DOS attack the FTP server
         start = time.time()
         print('Fetch async process {0} started at {1}'.format(pid, start))
 
         toCdDir = posixPath.parent  #[1:-1])   #the directory inside the ftp server to change_directory (cd) into
-        theFile = posixPath.name               #the filename which we wish to get
+        pathName = posixPath.name               #the folder/filename which we wish to get
 
         async with aioftp.ClientSession(siteURL, 21, user=usr, password=pwd) as client:
             await client.change_directory(toCdDir)        #change ftp directory to the folder we want
-            print("Downloading", theFile)
-            await client.download(theFile, pathlib.Path(dataDir, satName, toCdDir))                #finally downloads the file we want into specified folder
+            await smart_recursive_dwn(client, pathName, pathlib.Path(dataDir, satName, toCdDir))
 
             #for path, info in (await client.list(recursive=True)):
                 #print(path,info)
@@ -44,23 +62,23 @@ async def async_download(site, cdpath, usr, pwd):
 
     #Create tasks and run them concurrently via asyncio
     sem = asyncio.Semaphore(MAX_CLIENTS)                                                 #Pass in Semaphore FTP connection limit
-    tasks = [asyncio.ensure_future(fetch_async(sem, i, filename)) for i, filename in enumerate(filenames)]  #Create series of coroutines tasks (download tasks) to execute
+    tasks = [asyncio.ensure_future(fetch_async(sem, i, filename)) for i, filename in enumerate(filenames, start = 1)]  #Create series of coroutines tasks (download tasks) to execute
     await asyncio.wait(tasks)                                                   #Execute those coroutines concurrently and wait for them to fin
 
     print("Asynchronous process took: {:.2f} seconds".format(time.time() - start))
-
+    #1827.44s benchmark on 942 files
 
 for orgAccount, siteURL in ftpSites.items():
     #Manual hardcode to create local subdirectory name based on which server we get data from (E.g. cryosat if from ESA, icesat if from NSIDC, etc)
     if 'cryosat' in siteURL:
         satName = 'cryosat'
-        cdPath = pathlib.Path("SIR_GDR", "2017", "01")
+        cdPath = pathlib.Path("SIR_GDR", "2017")
     else:
         raise ValueError('unknown satellite')
 
     #Make directory if not already exist
-    pathlib.Path.mkdir(pathlib.Path(dataDir / satName / cdPath), parents=True, exist_ok=True)
-    pathlib.Path(dataDir / satName / cdPath).chmod(0o777)
+    pathlib.Path.mkdir(pathlib.Path(dataDir / satName / cdPath), mode=0o777, parents=True, exist_ok=True)
+    #pathlib.Path(dataDir / satName / cdPath).chmod(0o777)
 
     #Enter FTP login information
     '''
