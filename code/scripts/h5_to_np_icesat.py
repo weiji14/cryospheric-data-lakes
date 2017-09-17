9dcc8a0d5bee5cf81c6a6638fa79b4946e0ea6a7
Wei Ji <weiji14@users.noreply.github.com>
2017-09-18 11:08:31 +1200

    :art: Create h5_to_pydata function for reading hdf5 icesat
    Breaking scipt up into more reusable functions, one function to create a dictionary of fields we want to read, and another function that reads those fields from the actual HDF5 file. Output them into numpy, pandas and xarray formats.
    
    Considering using [dask](https://github.com/dask/dask) for more synchronous (think asyncio) way of reading multiple hdf5 files. Could try passing in a regex (e.g. GLAH12_634_*.H5) into the h5_to_pydata function and output a dask dataframe instead?

--- code/scripts/h5_to_np_icesat.py
+++ code/scripts/h5_to_np_icesat.py
@@ -11,71 +11,100 @@ print(os.getcwd())
 os.chdir('/home/atom/alp/data/icesat/GLAH12.034/2003.02.20')
 print(os.getcwd())
 
+# %%
+def init_h5_keyDict(h5file):
+    '''
+    Function to pick the fields we want from a HDF5 file.
+    Currently hardcoded for ICESAT GLAH12 HDF5 files.
 
-if 1==1:
-#with h5py.File("GLAH12_634_1102_001_0071_0_01_0001.H5", "r") as f:
-    f = h5py.File("GLAH12_634_1102_001_0071_0_01_0001.H5", "r")
-    f.name
-    f.libver
-    f.driver
+    Arguments:
+    h5file -- input data, of type HDF5 https://support.hdfgroup.org/HDF5/whatishdf5.html
 
+    Returns:
+    fields -- python dictionary with keys (e.g. x, y, z, t) mapped to values (fieldnames within the HDF5 file)
+    '''
+    h5 = h5py.File(h5file, "r")
+    assert(isinstance(h5, (h5py._hl.files.File)))
 
     #GLAH12 Product Data Dictionary https://nsidc.org/data/glas/data-dictionary-glah12
-    [k for k in f.keys()]
+    #[k for k in h5.keys()]
 
-    # %%
-    if isinstance(f, (h5py._hl.files.File)):
-        datagroup = "Data_40HZ"
-        [g for g in f['{0}'.format(datagroup)]]
+    #%% Retrieve data stored in the hdf5 file using known keys.
+    datagroup = "Data_40HZ"
+    [g for g in h5['{0}'.format(datagroup)]]
 
-        fields = collections.OrderedDict()
-        [v.name for v in f[datagroup].values()]
+    fields = collections.OrderedDict()
+    [v.name for v in h5[datagroup].values()]
 
-        #Standard parameters
-        fields['x'] = 'Geolocation/d_lon'
-        fields['y'] = 'Geolocation/d_lat'
-        fields['z'] = 'Elevation_Surfaces/d_elev'
-        fields['t'] = 'Time/d_UTCTime_40'
+    #Standard parameters
+    fields['x'] = 'Geolocation/d_lon'
+    fields['y'] = 'Geolocation/d_lat'
+    fields['z'] = 'Elevation_Surfaces/d_elev'
+    fields['t'] = 'Time/d_UTCTime_40'
 
-        #All other useful-ish parameters
-#        def func(name, obj):
-#            if isinstance(obj, h5py.Dataset):
-#                if obj.ndim == 1:
-#                    fields[name] = name
-#                else:
-#                    print("Warn: {0} is not one-dimensional, ignoring...".format(name))
-#        f[datagroup].visititems(func)
+    #All other useful-ish parameters
+    #def func(name, obj):
+    #    if isinstance(obj, h5py.Dataset):
+    #        if obj.ndim == 1:
+    #            fields[name] = name
+    #        else:
+    #            print("Warn: {0} is not one-dimensional, ignoring...".format(name))
+    #h5[datagroup].visititems(func)
 
-        #Finally create the n-dimensional array to store all the data. Do this by stacking each feature (e.g. coordinates/time/other param) using np.dstack.
-        dataListShape = [f[datagroup+'/'+fields[key]].shape for key in fields.keys()]
-        assert(np.median(dataListShape) == np.max(dataListShape))  #stupid way to get 'm' which is the no. of individual datapoints
-        m = np.max(dataListShape) #take m as the largest length
+    assert(isinstance(fields, dict))
+    return fields
 
-        ## numpy
-        xyzt = np.hstack((f[datagroup+'/'+fields[key]][:].reshape(-1,1) for key in fields.keys() if f[datagroup+'/'+fields[key]].shape == m)).T
-        assert(xyzt.shape == (len(fields), m))  #check that final numpy array has shape (n, m) where n is no. of features and m is no. of datapoints e.g. (4, 20000)
-        xyzt.shape
-        xyzt.ndim
-        xyzt.T.ndim
+def h5_to_pydata(h5file, h5fields):
+    '''
+    Function to load data from one HDF5 file to standard Python based formats.
+    Basically creates an n-dimensional array to store all the data. Do this by stacking each feature (e.g. coordinates/time/other param) using np.stack.
 
-        ## pandas
-        assert(isinstance(xyzt, np.ndarray))
-        pdData = pd.DataFrame(xyzt.T, columns=fields.keys())
-        pdData['t'] = pd.to_datetime(pdData['t'], unit='s', origin=pd.Timestamp('2000-01-01'), infer_datetime_format=True)  #convert time data into standard python datetime format
-        pdData
+    Arguments:
+    h5file -- input data, of type HDF5 https://support.hdfgroup.org/HDF5/whatishdf5.html
+    h5fields -- python dictionary containing keys (e.g. x, y, z, t) mapped to values (fieldnames within the HDF5 file)
 
-        pdData.loc[:,['x','y','z']].loc[lambda df: df.y < 0].to_csv("/home/atom/alp/code/scripts/pdData.csv")  #export Antarctic data (South of Equator) only
-        pdData.to_csv("/home/atom/alp/code/scripts/pdData.csv")
+    Returns:
+    xyzt  -- numpy.array of shape (n, m) where n is number of fields and m is number of datapoints
+    pdData -- pandas.DataFrame version of above numpy array, with the time (t) field/column expressed in standard python datetime format
+    xrData -- xarray.Dataset version of the above pandas.DataFrame which has dimensions (m) and data variables (n)
+    '''
+    h5 = h5py.File(h5file, "r")
+    assert(isinstance(h5, (h5py._hl.files.File)))
+    #h5.name
+    #h5.libver
+    #h5.driver
 
-        ##%%
-        assert(isinstance(pdData['t'][0], pd.Timestamp))
-        #help(pdData.loc)
+    #%% calculate m (number of individual datapoints) so we can do reshapes and assertion checks
+    dataListShape = [h5[datagroup+'/'+h5fields[key]].shape for key in h5fields.keys()]
+    assert(np.median(dataListShape) == np.max(dataListShape))  #stupid way to get 'm' which is the no. of individual datapoints
+    m = np.max(dataListShape) #take m as the largest length
 
-        ## xarray
-        assert(isinstance(pdData, pd.DataFrame))
-        xrData = pdData.to_xarray()
-        xrData
-        assert(isinstance(xrData, xr.Dataset))
+    #%% numpy
+    xyzt = np.hstack((h5[datagroup+'/'+h5fields[key]][:].reshape(-1,1) for key in h5fields.keys() if h5[datagroup+'/'+h5fields[key]].shape == m)).T
+    assert(xyzt.shape == (len(h5fields), m))  #check that final numpy array has shape (n, m) where n is no. of features and m is no. of datapoints e.g. (4, 20000)
+    xyzt.shape
+    xyzt.ndim
+    xyzt.T.ndim
+
+    #%% pandas
+    assert(isinstance(xyzt, np.ndarray))
+    pdData = pd.DataFrame(xyzt.T, columns=h5fields.keys())
+    pdData['t'] = pd.to_datetime(pdData['t'], unit='s', origin=pd.Timestamp('2000-01-01'), infer_datetime_format=True)  #convert time data into standard python datetime format
+    assert(isinstance(pdData['t'][0], pd.Timestamp))
+
+    #%% xarray
+    assert(isinstance(pdData, pd.DataFrame))
+    xrData = pdData.to_xarray()
+    xrData
+    assert(isinstance(xrData, xr.Dataset))
+
+    return xyzt, pdData, xrData
+
+h5fields = init_h5_keyDict("GLAH12_634_1102_001_0071_0_01_0001.H5")
+xyzt, pdData, xrData = h5_to_pydata("GLAH12_634_1102_001_0071_0_01_0001.H5", h5fields)
+pdData.loc[:,['x','y','z']].loc[lambda df: df.y < 0].to_csv("/home/atom/alp/code/scripts/pdData.csv")  #export Antarctic data (South of Equator) only
+pdData.to_csv("/home/atom/alp/code/scripts/pdData.csv")
+
 
 
     # %%
